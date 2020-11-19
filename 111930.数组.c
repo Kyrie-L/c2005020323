e0f0969e06937a76f673a10650704bdf061d3e0d
Kyrie-L <74295324+Kyrie-L@users.noreply.github.com>
2020-11-19 20:38:54 +0800

    Create 111930.数组.c

---
+++ 111930.数组.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+	int arr[]={2020,2021,2021,2023};
+	int a;
+	for(a=0;a<=3;a++){
+	printf("%d\n",arr[a]);}
+	scanf("%d\n",&a);
+	printf("%d\n",arr[a-1]);
+
+
+
+
+
+}
