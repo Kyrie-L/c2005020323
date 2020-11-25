9740f61f3eea20b637574b772c5669a4c98d677a
Kyrie-L <74295324+Kyrie-L@users.noreply.github.com>
2020-11-25 09:48:51 +0800

    Create 112502.QS排名查询.app.c

---
+++ 112502.QS排名查询.app.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+void main(){
+int p;
+printf("          大学排名查寻\n");
+scanf("%d",&p);
+if(p==1)
+{
+printf("清华大学");
+}
+else if(p==2)
+{
+printf("北京大学");
+}
+else if(p==3)
+{
+printf("复旦大学");
+}
+else if(p==4)
+{
+printf("中国科技大学");
+}
+else if(p==5)
+{
+printf("浙江大学");
+}
+else if(p==6)
+{
+printf("上海交通大学");
+}
+else if(p==7)
+{
+printf("南京大学");
+}
+else if(p==8)
+{
+printf("中山大学");
+}
+else if(p==9)
+{
+printf("武汉大学");
+}
+else if(p==10)
+{
+printf("哈尔滨工业大学");
+}
+else
+{
+printf("老铁大学排名太靠后了");
+}
+
+
+
+
+
+
+}
