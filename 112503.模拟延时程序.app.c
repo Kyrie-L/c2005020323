fa4073f2ff521b70166bfced6932a582cf84b22f
Kyrie-L <74295324+Kyrie-L@users.noreply.github.com>
2020-11-25 16:59:02 +0800

    Create 112503.模拟延时程序.app.c

---
+++ 112503.模拟延时程序.app.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<windows.h>
+void main(){
+	printf("男：早安，玛卡巴卡！\n");
+Sleep(2000);
+printf("女：早安，叮叮车\n");
+Sleep(2000);
+printf("男：今天天气不错哦\n");
+Sleep(2000);
+printf("女：不约哦！\n");
+Sleep(2000);
+printf("男：。。。。。\n");
+}
