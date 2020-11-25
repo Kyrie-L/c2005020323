cc0317b8a6de28acec244f9fbd477dd36da8ae3c
Kyrie-L <74295324+Kyrie-L@users.noreply.github.com>
2020-11-25 09:32:57 +0800

    Create 112501.打工人工资.app.c

---
+++ 112501.打工人工资.app.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main(){
+int money;
+scanf("%d",&money);
+if(money>=0&&money<=100)
+{
+printf("赤贫打工人");
+}
+else if(money>=101&&money<=1000)
+{
+printf("普通打工人");
+}
+else if(money>=1001&&money<=20000)
+{
+printf("土豪打工人");
+}
+else
+{
+printf("输入有误，请重新输入");
+}
+
+
+
+
+
+
+}
