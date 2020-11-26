d8f310c4427f3f1cf8ba0774a0904543619aaab2
Kyrie-L <74295324+Kyrie-L@users.noreply.github.com>
2020-11-26 21:05:24 +0800

    Create 112603.升序排列.app.c

---
+++ 112603.升序排列.app.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main(){
+int a,b,c,l;
+printf("输入三个整数");
+scanf("%d %d %d",&a,&b,&c);
+
+if(a>b)
+{
+	l=a;
+	a=b, b=l;
+}
+
+ if(a>c){
+	l=a;
+a=c,c=l;
+}
+if(b>c){
+	l=b;
+b=c,c=l;
+}
+printf("%d,%d,%d",a,b,c);
+}
