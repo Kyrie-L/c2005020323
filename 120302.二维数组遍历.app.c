6939378843ee10ddec5f38ab77f2ccd8a26027e6
Kyrie-L <74295324+Kyrie-L@users.noreply.github.com>
2020-12-03 20:56:07 +0800

    Create 120302.二维数组遍历.app.c

---
+++ 120302.二维数组遍历.app.c
@@ -0,0 +1,18 @@
+void main()
+{
+	int h,l;
+	
+	int arr2d[2][3]={
+		{1,2,3},
+		{4,5,6}
+					};
+	for(h=0;h<2;h++){
+	  for(l=0;l<3;l++)  {
+		printf("%d",arr2d[h][l]);
+						}
+			printf("\n");
+
+					}
+
+		
+}
