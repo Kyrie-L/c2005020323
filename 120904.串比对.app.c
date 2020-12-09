ccd1a3f7df45d01a9d75f09e3e6de29b415ea316
Kyrie-L <74295324+Kyrie-L@users.noreply.github.com>
2020-12-09 16:50:47 +0800

    Create 120904.串比对.app.c

---
+++ 120904.串比对.app.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char s1[]="打工人";
+
+	char s2[]="打工人";
+	if(strcmp(s1,s2)==0)
+	{
+	printf("相同\n");
+	
+	}
+else
+
+	printf("不相同\n");
+
+
+
+}
